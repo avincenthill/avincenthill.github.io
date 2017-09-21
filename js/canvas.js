5f3b591648ab1d6d5a03cc33b6bc684fb5f986c1
Alex Vincent-Hill <alex.vincent-hill@wearefetch.com>
2017-09-21 14:52:31 -0700

    add drawing elements to canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,26 @@
-/*jslint browser:true */
-
-var canvas = document.querySelector('canvas');
+var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+//define context variable for canvas
+var c = canvas.getContext('2d');
+
+/*
+c.beginPath();
+c.moveTo(0, 0);
+c.lineTo(1600, 1600);
+c.stroke();
+*/
+
+var i = 0;
+for (i = 0; i < 300; i += 1) {
+    var x = Math.floor(Math.random() * canvas.width);
+    var y = Math.floor(Math.random() * canvas.height);
+    c.beginPath();
+    c.arc(x, y, Math.floor(Math.random() * 10), 0, Math.PI * 2, false);
+    c.stroke();
+}
+
+//TBD: fix .png icons to remove whitespace
+//TBD: fix divs header/canvas/footer locations
+//TBD: fix div z-indexing

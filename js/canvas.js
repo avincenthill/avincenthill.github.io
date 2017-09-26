4988b19f82fc6cf8c6ea0f699014980738299ecc
Alex Vincent-Hill <alex.vincent-hill@wearefetch.com>
2017-09-25 17:45:03 -0700

    add html canvas trees, remove footer

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,7 @@
+//TBD: fix on-hover effects
+//TBD: remove whitespace on vector logos
+
+
 var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -12,6 +16,7 @@ c.lineTo(1600, 1600);
 c.stroke();
 */
 
+/*
 var i = 0;
 for (i = 0; i < 300; i += 1) {
     var x = Math.floor(Math.random() * canvas.width);
@@ -20,7 +25,37 @@ for (i = 0; i < 300; i += 1) {
     c.arc(x, y, Math.floor(Math.random() * 10), 0, Math.PI * 2, false);
     c.stroke();
 }
+*/
 
-//TBD: fix .png icons to remove whitespace
-//TBD: fix divs header/canvas/footer locations
-//TBD: fix div z-indexing
+//TBD: make this object oriented
+function Tree(x, y, lwidth){
+    this.x = x;
+    this.y = y;
+    this.lwidth = lwidth;
+    
+    this.grow = function () {
+
+        c.beginPath();
+        c.moveTo(x, y);
+        c.lineTo(x, y);
+        c.stroke();
+        c.lineWidth = lwidth;
+        c.lineCap = 'round';
+        c.lineJoin = 'round';
+
+        x -= (Math.random() - .5);
+        y -= Math.random();
+        lwidth -= Math.random() / 10;
+        
+    }
+}
+
+var tree1 = new Tree(canvas.width * (1 / 5), canvas.height, 50);
+var tree2 = new Tree(canvas.width * (4 / 5), canvas.height, 50);
+
+function animate() {
+    requestAnimationFrame(animate);
+    tree1.grow();
+    tree2.grow(); 
+}
+animate();
